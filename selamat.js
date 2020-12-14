1f1817352894d0d18d339882fe962a26f9e5ad01
IlfiNurDiana <70804808+IlfiNurDiana@users.noreply.github.com>
2020-12-14 17:07:36 +0700

    Add files via upload

---
+++ selamat.js
@@ -0,0 +1,19 @@
+document.getElementById("ucap");
+    var a=prompt("Ketikkan namamu!")
+    var b=new Date();
+    var c=b.getHours();
+    if(c>0 && c<9){
+        document.write("Selamat pagi" + " " + a + "." + " " + "Jangan lupa sarapan ya. Semangat belajar." + " " + "Sejuta merpati tak akan pernah terbang bila tidak mau belajar. Berjuta bintang tidak pernah bersinar jika setiap saat selalu siang.");
+    }
+    else if(c>=0 && c<9){
+        document.write("Selamat pagi" + " " + a + "." + " " + "Jangan lupa sarapan ya. Semangat belajar." + " " + "Sejuta merpati tak akan pernah terbang bila tidak mau belajar. Berjuta bintang tidak pernah bersinar jika setiap saat selalu siang.");
+    }
+    else if(c>=9 && c<16){
+        document.write("Selamat siang" + " " + a + "." + " " + "Tetap semangat dalam menjalankan aktivitas. Semangat belajar." + " " + "Sejuta merpati tak akan pernah terbang bila tidak mau belajar. Berjuta bintang tidak pernah bersinar jika setiap saat selalu siang.");
+    }
+    else if(c>=16 && c<18){
+        document.write("Selamat sore" + " " + a + "." + " " + "Jangan lupa istirahat ya. Semangat belajar." + " " + "Sejuta merpati tak akan pernah terbang bila tidak mau belajar. Berjuta bintang tidak pernah bersinar jika setiap saat selalu siang.");
+    }
+    else if(c>=18 && c<24){
+        document.write("Selamat malam" + " " + a + "." + " " + "Jangan lupa tidur ya. Semangat belajar." + " " + "Sejuta merpati tak akan pernah terbang bila tidak mau belajar. Berjuta bintang tidak pernah bersinar jika setiap saat selalu siang.");
+    }
